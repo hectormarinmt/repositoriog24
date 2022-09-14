c908ff10d82253f18312eaa96cad2cbfd7123815
Hector Marin <hector.marin.mt@usa.edu.co>
2022-09-14 18:59:58 -0500

    agregando funcionalidad en backend

---
+++ src/main/java/com/ciclo3/proyectog24/Ejercicio4.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.ciclo3.proyectog24;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ejercicio4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     System.out.print("Hola a todos ejercicio 4");
+    }
+    
+}
